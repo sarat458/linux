f661cd6c0ce072bd7cc8fa406cddf38b969be958
sarat458 <srthkmr491@gmail.com>
2022-04-29 22:07:25 +0000

    created Assignment3 & Assignment4 folders

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -6001,6 +6001,7 @@ void dump_vmcs(struct kvm_vcpu *vcpu)
 		       vmcs_read16(VIRTUAL_PROCESSOR_ID));
 }
 
+u32 total_exits;
 /*
  * The guest has exited.  See if we can fix it or if we need userspace
  * assistance.
@@ -6011,7 +6012,9 @@ static int __vmx_handle_exit(struct kvm_vcpu *vcpu, fastpath_t exit_fastpath)
 	union vmx_exit_reason exit_reason = vmx->exit_reason;
 	u32 vectoring_info = vmx->idt_vectoring_info;
 	u16 exit_handler_index;
-
+	
+	total_exits = total_exits + 1;
+	
 	/*
 	 * Flush logged GPAs PML buffer, this will make dirty_bitmap more
 	 * updated. Another good is, in kvm_vm_ioctl_get_dirty_log, before
